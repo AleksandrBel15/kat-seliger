259dbf7837b2af969ed59a2d10094296bb48e717
Aleksandr <aleks.belov.08@mail.ru>
2025-06-20 19:19:02 +0300

    feat: Done Hero-block with responsive design and animations.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,13 +9,16 @@ export default class Hero {
         const section = document.createElement('section');
         section.className = 'hero';
         section.innerHTML = `
-        <div class = "hero__content">
-            <h1>Клуб активного туризма <br> Селигер</h1>
-            <p class="hero__subtitle">Присоединяйтесь, мы вам рады!</p>
-        </div>
-        <button class="hero__scroll-down">
-            <span class="hero__scroll-arrow"></span>
-        </button>
+            <div class = "hero__content">
+                <h1>Клуб активного туризма <br> Селигер</h1>
+                <p class="hero__subtitle">Присоединяйтесь, мы вам рады!</p>
+            </div>
+        
+            <button class="hero__scroll-down" aria-label="Прокрутить вниз">
+                <svg class="hero__arrow" width="38" height="19" viewBox="0 0 38.417 18.592" fill="#ffffff">
+                    <path d="M19.208,18.592c-0.241,0-0.483-0.087-0.673-0.261L0.327,1.74c-0.408-0.372-0.438-1.004-0.066-1.413c0.372-0.409,1.004-0.439,1.413-0.066L19.208,16.24L36.743,0.261c0.411-0.372,1.042-0.342,1.413,0.066c0.372,0.408,0.343,1.041-0.065,1.413L19.881,18.332C19.691,18.505,19.449,18.592,19.208,18.592z"/>
+                </svg>
+            </button>
         `;
         return section;
     }
