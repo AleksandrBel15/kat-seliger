fe20b5d9d6f8173cb3208343432894aff2a7190e
Aleksandr <aleks.belov.08@mail.ru>
2025-07-01 12:13:04 +0300

    feat: About component

--- src/components/AboutUs/About.js
+++ src/components/AboutUs/About.js
@@ -8,12 +8,13 @@ export default class About {
     render() {
         const sectionAbout = document.createElement('section');
         sectionAbout.className = 'about';
+        sectionAbout.id = 'about';
         sectionAbout.innerHTML = `
-            <h2 class="about__title">О нас</h2>
-            <div class="about__cards flex-center">
+            <h2 class="about__title ubuntu-medium">О нас</h2>
+            <div class="about__cards flex-center ubuntu-medium">
                 <div class="about__card flex-center">Клуб Активного Туризма "Селигер" (КАТ "Селигер") - для тех, кто любит активный отдых, весёлую компанию, путешествия, приключения и озеро Селигер.</div>
-                <div class="about__card flex-center">Мы занимаемся разработкой и популяризацией активных маршрутов. Наши Походы можно забронировать у наших партнеров в Москве и Санкт-Петербурге.</div>
-                <div class="about__card flex-center">В городе Осташков мы приглашаем детей и подростков в "Туристический кружок".</div>
+                <div class="about__card flex-center">Мы занимаемся разработкой и популяризацией активных маршрутов. <br> Наши Походы можно забронировать у наших партнеров в Москве и Санкт-Петербурге.</div>
+                <div class="about__card flex-center">В городе Осташков мы приглашаем детей и подростков в <br> "Туристический кружок".</div>
             </div>
         `;
 
