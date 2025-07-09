96a291a25bc5b31d4e018fe270833ac4bf188ad6
AleksandrBel15 <150532685+AleksandrBel15@users.noreply.github.com>
2025-07-09 19:52:55 +0300

    style: add responsive design

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -16,7 +16,7 @@ export default class Gallery {
         sectionGallery.className = 'gallery';
         sectionGallery.innerHTML = `
             <h2>А это мы!</h2>
-            <div class="imageWrapper">
+            <div class="imageWrapper flex-center">
                 <button class="gallery__button left-button" id="left-button" aria-label="Изображение назад">
                     <svg width="38" height="19" viewBox="0 0 38.417 18.592" fill="#ffffff">
                         <path d="M19.208,18.592c-0.241,0-0.483-0.087-0.673-0.261L0.327,1.74c-0.408-0.372-0.438-1.004-0.066-1.413c0.372-0.409,1.004-0.439,1.413-0.066L19.208,16.24L36.743,0.261c0.411-0.372,1.042-0.342,1.413,0.066c0.372,0.408,0.343,1.041-0.065,1.413L19.881,18.332C19.691,18.505,19.449,18.592,19.208,18.592z"/>
