c69da416833bc3a0485e3bf8fa697a733648b059
Aleksandr <aleks.belov.08@mail.ru>
2025-07-11 17:58:39 +0300

    feat: Customer Reviews

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,11 @@ import About from '../components/AboutUs/About.js';
 const about = new About();
 import Gallery from '../components/Gallery/Gallery.js';
 const gallery = new Gallery();
+import Reviews from '../components/Reviews/Reviews.js';
+const reviews = new Reviews();
 
 const app = document.getElementById('app');
 app.appendChild(hero.element);
 app.appendChild(about.element);
 app.appendChild(gallery.element);
-
+app.appendChild(reviews.element);
