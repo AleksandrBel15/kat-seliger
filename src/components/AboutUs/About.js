975ddd42d8847c6d84a2671ae213f509ce8a0c88
Aleksandr <aleks.belov.08@mail.ru>
2025-07-11 13:12:24 +0300

    style: fix responsive design

--- src/components/AboutUs/About.js
+++ src/components/AboutUs/About.js
@@ -10,7 +10,7 @@ export default class About {
         sectionAbout.className = 'about';
         sectionAbout.classList.add('flex-center');
         sectionAbout.innerHTML = `
-            <h2 class="about__title ubuntu-medium">О нас</h2>
+            <h2 class="about__title flex-center ubuntu-medium">О нас</h2>
             <div class="about__cards flex-center ubuntu-medium">
                 <div class="about__card flex-center text-center">Клуб Активного Туризма "Селигер" (КАТ "Селигер") - для тех, кто любит активный отдых, весёлую компанию, путешествия, приключения и озеро Селигер.</div>
                 <div class="about__card flex-center text-center">Мы занимаемся разработкой и популяризацией активных маршрутов. <br> Наши Походы можно забронировать у наших партнеров в Москве и Санкт-Петербурге.</div>
