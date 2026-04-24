a8f4f9e1852715e784420fe598b0405ef52c24d2
AleksandrBel15 <aleks.belov.08@mail.ru>
2026-04-24 23:58:34 +0300

    restruct

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-    root: './src', 
+    root: '.', 
     publicDir: './public',
     build: {
         outDir: './dist',
